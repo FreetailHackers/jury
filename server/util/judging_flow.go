a51f669cde27654bb053ce6dc1487713d5934b54
Aditya <adityaagrawal200313@gmail.com>
2023-10-18 23:57:30 -0500

    missing comma in print statement

--- server/util/judging_flow.go
+++ server/util/judging_flow.go
@@ -97,7 +97,7 @@ func FindPreferredItems(db *mongo.Database, judge *models.Judge) ([]*models.Proj
 
 	// NEW - Filter projects based on the judges locality
 	var localityProjects []*models.Project
-	fmt.Println("Starting Localities: " judge.CurrentLocalities)
+	fmt.Println("Starting Localities: ", judge.CurrentLocalities)
 	if judge.LocalityTableCount > LocalityTableMax || len(judge.CurrentLocalities) == 0 {
 		judge.LocalityTableCount = 0
 		if len(judge.CurrentLocalities) == len(Localities) {
